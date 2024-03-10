b822a1cfc5f542a64096c2ab24429dc559b63058
Caduzinhok <carlos_fire1@outlook.com>
2024-03-10 11:55:14 -0300

    feat: changes recipe page

--- app/recipe/recipe.tsx
+++ app/recipe/recipe.tsx
@@ -11,14 +11,14 @@ interface PropsRecipe {
 
 export function Recipe(PropsRecipe: PropsRecipe) {
     return (
-        <main className="flex items-center justify-between w-full mt-8 py-8 rounded-md bg-lime-300">
+        <main className="flex items-center justify-between w-full mt-8 py-8 rounded-3xl border-red-300 border-2 border-x-[30px] border-x-red-500">
             <div className="grid grid-cols-2 w-full">
-                <div className="flex items-center justify-center p-4">
-                    Images Carrousel
+                <div className="flex items-center justify-center p-1 rounded-xl ml-2">
+                    <img src={PropsRecipe.image} alt={PropsRecipe.titulo} className="w-full rounded-md"/>
                 </div>
 
-                <div className="py-4">
-                    <h1 className="text-2xl text-slate-800">
+                <div className="py-2 p-4">
+                    <h1 className="text-2xl text-white pl-2 bg-slate-400">
                         {PropsRecipe.titulo}
                     </h1>
                     <p className="text-lg text-slate-500">
